9881abf03027c42e15ef79b99702c68df808927e
DraghiciFlavia <55551803+DraghiciFlavia@users.noreply.github.com>
2019-10-28 19:22:18 +0200

    ex

--- Tema4PinteriSpreHeap/Tema4PointeriSpreHeap.cpp
+++ Tema4PinteriSpreHeap/Tema4PointeriSpreHeap.cpp
@@ -1,23 +1,23 @@
-// Tema4_Su,a Tablouri.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Tema4PointeriSpreHeap.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
-using namespace std;
-short* arraySuma = new short[41];
+double* pdouble = new double(1.3);
 int main()
 {
-	short arrayStack[41];
-	short* arrayHeap = new short[41];
-	for (short i = 0; i < 41; ++i)
-	{
-		arrayStack[i] = 7 * i;
-		arrayHeap[i] = 8 * i;
-		arraySuma[i] = arrayStack[i] + arrayHeap[i];
-		cout << "Elem[" << i << "]: " << arraySuma[i] << endl;
-	}
-	delete[] arrayHeap;
-	arrayHeap[]{ 0 };
+	double* pDouble = new double(2.3);
+	double* pDouble1 = new double(5.4);
+	double* pntMedie = new double;
+	*pntMedie = (*pdouble + *pDouble + *pDouble1) / 3;
+	std::cout << *pntMedie << std::endl;
+	delete pDouble;
+	delete pDouble1;
+	delete pntMedie;
+	pDouble = nullptr;
+	pDouble1 = nullptr;
+	pntMedie = nullptr;
 }
+
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
